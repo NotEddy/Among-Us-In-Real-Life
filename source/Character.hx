1cbca1949d058a47fd9ec21ab13218c6b6e74d22
NotEddy <eddysdumbedits@gmail.com>
2022-05-07 17:50:57 -0700

    Yankin

--- source/Character.hx
+++ source/Character.hx
@@ -91,7 +91,7 @@ class Character extends FlxSprite
 		var library:String = null;
 		switch (curCharacter)
 		{
-			//case 'your character name in case you want to hardcode him instead':
+			//case 'bf-igor':
 
 			default:
 				var characterPath:String = 'characters/' + curCharacter + '.json';
